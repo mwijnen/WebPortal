be091379fed213c28be3dd8514aa11bd0adedc8e
Marcel Wijnen <marcel.wijnen@outlook.com>
2020-11-15 10:24:41 -0800

    US0001ImplementingIdentity-small-update

--- WebPortal/Controllers/AccountController.cs
+++ WebPortal/Controllers/AccountController.cs
@@ -158,16 +158,6 @@ namespace WebPortal.Controllers
             return RedirectToAction(nameof(HomeController.Index), "Home");
         }
 
-        //Register (navigate to form and initialize empty registration)
-
-        //Register (handle form submission, create new user, redirect)
-
-        //Verify email action
-
-        //Login (navigate to form)
-
-        //Login (handle form submission, create new session, redirect)
-
         //Forgot password (navigate to form)
 
         //Forgot password (handle form submission, redirect)
@@ -176,6 +166,5 @@ namespace WebPortal.Controllers
 
         //Reset password (handle form submission, redirect)
 
-        //Logout
     }
 }
